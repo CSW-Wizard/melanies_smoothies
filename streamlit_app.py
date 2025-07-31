852b053497260b639b33be6cb885ad4984449a9c
CSW-Wizard <account@stevenshouse.co.uk>
2025-07-31 12:24:55 +0100

    Update streamlit_app.py
    data frame api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,5 @@ if ingredients_list:
 # New section to disply fruityvice.com nutrition information
 import requests
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use__container__width=True)
